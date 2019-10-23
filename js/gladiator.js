b1a4fb087394e6a0c9e9ecc5674f010805edeb0c
Muhannad Alanazi <muhannad.h.alanazi@gmail.com>
2019-10-23 18:34:03 +0300

    Done

---
+++ js/gladiator.js
@@ -0,0 +1,9 @@
+export default class Gladiator{
+    constructor(name, weapon){
+        this.name = name;
+        if(weapon == "Spear" || weapon == "Trident" || "Clib")
+        this.weapon = weapon
+        else console.log("Invalid weapon")
+    }
+    
+}
---
+++ js/index.js
@@ -0,0 +1,9 @@
+import Arena from "./arena.js"
+import Gladiator from "./gladiator.js"
+const max = new Gladiator("Maximus","Trident");
+const titus = new Gladiator("Titus","Spear");
+const colosseum = new Arena("Colosseum");
+
+colosseum.addGladiator(max);
+colosseum.addGladiator(titus);
+colosseum.fight();  
